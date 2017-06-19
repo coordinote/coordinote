fba7b78b474d73586c13ddbda0beb74dfae24b45
Mairu Matsuyama <maturu@MairunoMacBook-Pro.local>
2017-06-20 02:42:32 +0900

    add tolerance & fix svg-data

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,11 @@
 //get canvas id 
 var container = $('#canvas')
 
+$('#omit').change(() => {
+    $('#omit-label').html($('#omit').val())
+})
+$('#omit-label').html($('#omit').val())
+
 //judge draw
 var isDrawing = false
 //mouse point
@@ -35,7 +40,7 @@ container.mousemove((e) => {
             drawingPath.remove()
         }
         //create path
-        drawingPath = createPath(drawingPoints, parseFloat(1.5), true)
+        drawingPath = createPath(drawingPoints, parseFloat($('#omit').val()), true)
         //path style setting
         Object.assign(drawingPath.style, defaultPathStyle)
         //add svg path to canvas
@@ -53,14 +58,19 @@ container.mouseup((e) => {
     drawingPath = null
     //svg data
     $('#data').empty()
-    var svg_data = $('#canvas').html().length
-    $('#data').append(svg_data*3 + "Byte")
+    var svg_data = $('#canvas').html().size()
+    $('#data').append(svg_data + "Byte")
 })
 
 //create path
 function createPath(points, tolerance, highestQuality) {
-    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    var attribute = SVGCatmullRomSpline.toPath(points.map(point => [point.x, point.y]), tolerance, highestQuality);
-    path.setAttributeNS(null, 'd', attribute);
-    return path;
+    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+    var attribute = SVGCatmullRomSpline.toPath(points.map(point => [point.x, point.y]), tolerance, highestQuality)
+    path.setAttributeNS(null, 'd', attribute)
+    return path
+}
+
+//svg-data size function
+String.prototype.size = function() {
+  return (new Blob([this], {type: "text/plain"})).size
 }
