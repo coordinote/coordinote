0659821ecc9b661888ee0b0db56eb66f75e660d4
maturu <maturu.410@gmail.com>
2017-06-21 01:42:56 +0900

    fix variable name

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,3 @@
-//get canvas id 
-var container = $('#canvas')
 //tools tolerance
 $('#omit').change(() => {
     $('#omit-label').html($('#omit').val())
@@ -7,27 +5,27 @@ $('#omit').change(() => {
 $('#omit-label').html($('#omit').val())
 
 //judge draw
-var isDrawing = false
+var isdraw = false
 //mouse point
-var drawingPoints
+var drawpoints
 //svg path
-var drawingPath
+var drawpath
 //svg path style
-var defaultPathStyle = {
+var pathstyle = {
     strokeWidth: "3px",
     stroke: "#000000",
     fill: "none"
 }
 
 //before and after stack
-var array = []
+var history_array = []
 
 //path style color
 $('#red').click(() => {
-    defaultPathStyle.stroke = "#ff0000"
+    pathstyle.stroke = "#ff0000"
 })
 $('#black').click(() => {
-    defaultPathStyle.stroke = "#000000"
+    pathstyle.stroke = "#000000"
 })
 
 //path before
@@ -35,62 +33,62 @@ $('#before').click(() => {
     //path delete and save
     var save_path = $('path:last').detach()
     //array include savepath and not undefined 
-    if(array.indexOf(save_path.get()[0]) != 0){
-        if(save_path.get()[0] != undefined){
+    if(history_array.indexOf(save_path.get()[0]) != 0){
+        if(save_path.get()[0] !== undefined){
             //add path in stack array
-            array.push(save_path.get()[0])
+            history_array.push(save_path.get()[0])
         }
     }
 })
 //path after
 $('#after').click(() => {
     //path restoration
-    container.append(array.getLastVal())
+    $('#canvas').append(history_array.getLastVal())
     //delete path in stack array
-    array.pop()
+    history_array.pop()
 })
 
 //mouse click
-container.mousedown((e) => {
+$('#canvas').mousedown((e) => {
     //judge true
-    isDrawing = true
+    isdraw = true
     //point array definition
-    drawingPoints = []
+    drawpoints = []
 })
 //mouse move
-container.mousemove((e) => {
+$('#canvas').mousemove((e) => {
     //draw true
-    if (isDrawing) {
+    if (isdraw) {
         //add mouse point
-        drawingPoints.push({
+        drawpoints.push({
             x: e.clientX,
             y: e.clientY
         })
         //drawingPath check existence
-        if (drawingPath) {
-            drawingPath.remove()
+        if (drawpath) {
+            drawpath.remove()
         }
         //create path
-        drawingPath = createPath(drawingPoints, parseFloat($('#omit').val()), true)
+        drawpath = createPath(drawpoints, parseFloat($('#omit').val()), true)
         //path style setting
-        Object.assign(drawingPath.style, defaultPathStyle)
+        Object.assign(drawpath.style, pathstyle)
         //add svg path to canvas
-        container.append(drawingPath)
+        $('#canvas').append(drawpath)
     }
 })
 //mouse up
-container.mouseup((e) => {
+$('#canvas').mouseup((e) => {
     //judge false
-    isDrawing = false
+    isdraw = false
     //drawingPath null or undefined
-    if (!drawingPath) {
+    if (!drawpath) {
         return
     }
-    drawingPath = null
+    drawpath = null
     //svg data
-    $('#data').empty()
+    $('#datasize').empty()
     var svg_data = $('#canvas').html().size()
-    $('#data').append(svg_data + "Byte")
+    $('#datasize').append(svg_data + "Byte")
 })
 
 //create path
@@ -106,6 +104,6 @@ String.prototype.size = function() {
   return (new Blob([this], {type: "text/plain"})).size
 }
 
-Array.prototype.getLastVal = function (){ 
+Array.prototype.getLastVal = function() {
     return this[this.length -1]
 }
