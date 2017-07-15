b2b7cc5306b5ec047703356b9fcd7aa3f33cc1be
maturu <maturu.410@gmail.com>
2017-07-16 03:31:04 +0900

    fix currentdate

--- export_screen/js/dateform.js
+++ export_screen/js/dateform.js
@@ -1,5 +1,6 @@
 let date_instance = new Date()
-let currentdate = date_instance.getMonth()+1+'/'+date_instance.getDay()+'/'+date_instance.getFullYear()
+let currentdate = date_instance.getMonth()+1+'/'+date_instance.getDate()+'/'+date_instance.getFullYear()
+console.log(currentdate)
 
 $('.start').val(currentdate)
 $('.end').val(currentdate)
