e8903322ad55f1f0fc22cf93a44da7a6d1ddad27
rikyuusima <equanz324@gmail.com>
2017-08-01 17:45:58 +0900

    fix type(to integer)

--- server/app.js
+++ server/app.js
@@ -175,12 +175,12 @@ io.sockets.on('connection',(socket) => {
   })
 
   socket.on('update_tilecol',(rec) => {
-    nedb.update_tilecol_cidid(rec.col,rec.cid,rec.tid,() => {
+    nedb.update_tilecol_cidid(parseInt(rec.col),rec.cid,rec.tid,() => {
     })
   })
 
   socket.on('update_tileidx',(rec) => {
-    nedb.update_tileidx_cidid(rec.idx,rec.cid,rec.tid,() => {
+    nedb.update_tileidx_cidid(parseInt(rec.idx),rec.cid,rec.tid,() => {
     })
   })
 
