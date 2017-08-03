c7b3bdf51d5b4fbb89c55679929f88f615266bfb
Syaz0326 <tadakazu326@gmail.com>
2017-08-03 13:35:39 +0900

    date-picker theme変更

--- edit_screen/write/app/app.component.ts
+++ edit_screen/write/app/app.component.ts
@@ -271,8 +271,8 @@ export class WriteNav{
     <!-- サイドバー(クリップ) -->
     <article class="clip-bar">
       <tag-input class="clip-view-tagsinput-content" [ngClass]="error ? 'error':''" [(ngModel)]="find_tag" [theme]="'bootstrap'" [placeholder]="tagsinput_placeholder" [secondaryPlaceholder]="tagsinput_placeholder" (change)="errorCancel($event)" (onBlur)="cliptagsubstitute(find_tag)"></tag-input>
-      <dp-date-picker class="clip-view-content" [(ngModel)]="date.start" [config]="datePickerConfig"></dp-date-picker>
-      <dp-date-picker class="clip-view-content" [(ngModel)]="date.end" [config]="datePickerConfig"></dp-date-picker>
+      <dp-date-picker class="clip-view-content" [(ngModel)]="date.start" theme="dp-material" [config]="datePickerConfig"></dp-date-picker>
+      <dp-date-picker class="clip-view-content" [(ngModel)]="date.end" theme="dp-material" [config]="datePickerConfig"></dp-date-picker>
       <button (click)="search()">search</button>
       <div *ngFor="let clip of clips">
         <button (click)="load_tile(clip)">{{clip._id}}</button>
